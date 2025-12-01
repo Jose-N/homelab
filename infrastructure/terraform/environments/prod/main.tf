da1b2228f3d5d8415011caf3faead55fd0b1d071
Jose Naylor <jose.c.naylor@gmail.com>
2025-12-01 00:00:45 -0500

    Updated komodo vm memory, added role to install docker

--- infrastructure/terraform/environments/prod/main.tf
+++ infrastructure/terraform/environments/prod/main.tf
@@ -4,7 +4,7 @@ module "komodo" {
   name            = "komodo"
   target_node     = var.target_node
   cores           = 1
-  memory          = 1024
+  memory          = 2048
   ipconfig0       = "ip=192.168.1.10/24,gw=192.168.1.1,ip6=dhcp"
   cipassword      = var.komodo_vm_pass
 }
